8205180bceb040bad61a31f893e47078bcc88965
Linda Kaioh <lindaoneesama@gmail.com>
2023-05-29 18:35:06 +0300

    Tweaked env file location

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Installing /usr/bin/finger2020"
+echo "Installing main executable..."
 install finger2020 /usr/bin/finger2020
 
 echo "Installing systemd files..."
 install systemd/finger2020.socket /etc/systemd/system/finger2020.socket
 install systemd/finger2020@.service /etc/systemd/system/finger2020@.service
-install -b systemd/finger2020.env /etc/sysconfig/finger2020.env
+install -b systemd/finger2020.env /etc/default/finger2020.env
 
 echo "Enabling systemd service..."
 systemctl daemon-reload
